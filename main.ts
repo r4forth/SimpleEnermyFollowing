1b20c8240feefbe35d0ee056a522c319bdc912e3
r4forth <r4forth@gmail.com>
2024-08-18 11:24:30 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,114 @@
+function 加入隨機遊蕩的行為 (sprite: Sprite) {
+    sprite.setStayInScreen(true)
+    spriteutils.onSpriteUpdateInterval(sprite, 500, function (sprite) {
+        if (Math.percentChance(50)) {
+            if (Math.percentChance(50)) {
+                sprite.setVelocity(50, 0)
+            } else {
+                sprite.setVelocity(-50, 0)
+            }
+        } else {
+            if (Math.percentChance(50)) {
+                sprite.setVelocity(0, 50)
+            } else {
+                sprite.setVelocity(0, -50)
+            }
+        }
+    })
+}
+function 加入何時關閉跟隨的行為 (sprite: Sprite, toFollow: Sprite) {
+    spriteutils.onSpriteUpdateInterval(sprite, 500, function (sprite) {
+        if (spriteutils.distanceBetween(sprite, toFollow) < 40) {
+            sprite.follow(toFollow, 50)
+        } else {
+            sprite.follow(toFollow, 0)
+        }
+    })
+}
+let 小鴨 = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(小鴨)
+let 亂走的蛇 = sprites.create(img`
+    . . . . c c c c c c . . . . . . 
+    . . . c 6 7 7 7 7 6 c . . . . . 
+    . . c 7 7 7 7 7 7 7 7 c . . . . 
+    . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+    . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+    . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+    . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+    . . f 7 7 7 7 6 c 7 7 6 f c . . 
+    . . . f c c c c 7 7 6 f 7 7 c . 
+    . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+    . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+    c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+    f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+    . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+    . . c c c c c c c c c f . . . . 
+    `, SpriteKind.Enemy)
+亂走的蛇.setPosition(randint(10, 150), randint(10, 110))
+加入隨機遊蕩的行為(亂走的蛇)
+let 亂走的骷髏 = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+亂走的骷髏.setPosition(142, 103)
+加入何時關閉跟隨的行為(亂走的骷髏, 小鴨)
+let 亂飛的蝙蝠 = sprites.create(img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c 1 b b b 1 b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b 1 f f 1 c b b b b f . . . . 
+    f f 1 f f 1 f b b b b f c . . . 
+    f f 2 2 2 2 f b b b b f c c . . 
+    . f 2 2 2 2 b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, SpriteKind.Enemy)
+加入何時關閉跟隨的行為(亂飛的蝙蝠, 小鴨)
+亂飛的蝙蝠.setPosition(7, 7)
